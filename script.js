21147284e468470ef5bbea701045160a715fcee0
Innuendo416 <collinstone2291@icloud.com>
2021-03-03 12:12:33 -0500

    Alignment button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const banner = document.querySelector('.banner')
 const bannerContent = document.getElementById('bannerContent')
 const bannerImage = document.getElementById('bannerImage')
 const bannerButton = document.getElementById('bannerButton')
@@ -5,8 +6,8 @@ const bannerStyleSwitch = document.getElementById('bannerStyleSwitch')
 const alignmentStyleSwitch = document.getElementById('bannerAlignmentSwitch')
 const bannerStyles = [
 	{
-		content: 'content--foreground',
-		image: 'image--background',
+		content: ['content--foreground'],
+		image: ['image--background']
 	},
 	{
 		content: ['content--foreground', 'content--foreground--alt'],
@@ -16,51 +17,78 @@ const bannerStyles = [
 
 const alignmentStyles = [
 	{
-		content: ''
-	}
+		content: ['content--alignCenter'],
+		button: ['content__button--alignCenter']
+	},
+	{
+		content: ['content--alignEnd'],
+		button: ['content__button--alignRight']
+	},
 ]
 
-let styleCounter = 0
-const handleStyle = (styles, element1, element2) => {
 
-	const removeClass = (element1, element2, index) => {
-		if(typeof styles[index][Object.keys(styles[index])[0]] !== 'string'){
-			styles[index][Object.keys(styles[index])[0]].forEach((c, i) => {
-				element1.classList.remove(c)
-			})
-			styles[index][Object.keys(styles[index])[1]].forEach((c, i) => {
-				element2.classList.remove(c)
-			})
-		}else {
-			element1.classList.remove(styles[index][Object.keys(styles[index])[0]])
-			element2.classList.remove(styles[index][Object.keys(styles[index])[1]])
-		}
-	}
-	const addClass = (element1, element2, index) => {
-		if(typeof styles[index][Object.keys(styles[index])[0]] !== 'string'){
-			styles[index][Object.keys(styles[index])[0]].forEach((c, i) => {
-				element1.classList.add(c)
-			})
-			styles[index][Object.keys(styles[index])[1]].forEach((c, i) => {
-				element2.classList.add(c)
-			})
-		}else {
-			element1.classList.add(styles[index][Object.keys(styles[index])[0]])
-			element2.classList.add(styles[index][Object.keys(styles[index])[1]])
-		}
-	}
 
-	if(styleCounter < styles.length){
-		if(styleCounter !== 0){
-			removeClass(element1, element2, styleCounter - 1)		
+let bannerStyleCounter = 0
+let bannerAlignmentCounter = 0
+const handleStyle = (styles, element1, element2, counter) => {
+	const removeClass = (element1, element2, style1, style2) => {
+		style1.forEach((c, i) => {
+			element1.classList.remove(c)
+		})
+		style2.forEach((c, i) => {
+			element2.classList.remove(c)
+		})
+
+	}
+	const addClass = (element1, element2, style1, style2) => {
+		style1.forEach((c, i) => {
+			element1.classList.add(c)
+		})
+		style2.forEach((c, i) => {
+			element2.classList.add(c)
+		})
+	}
+	if(counter < styles.length){
+		if(counter !== 0){
+			removeClass(
+				element1, 
+				element2, 
+				styles[counter-1][Object.keys(styles[counter-1])[0]], 
+				styles[counter-1][Object.keys(styles[counter-1])[1]]
+			)		
 		}
-		addClass(element1, element2, styleCounter)
-		styleCounter++
+		addClass(
+				element1, 
+				element2, 
+				styles[counter][Object.keys(styles[counter])[0]], 
+				styles[counter][Object.keys(styles[counter])[1]]
+			)
 	} else {
-		removeClass(element1, element2, styleCounter - 1)
-		styleCounter = 0
+		removeClass(
+				element1, 
+				element2, 
+				styles[counter-1][Object.keys(styles[counter-1])[0]], 
+				styles[counter-1][Object.keys(styles[counter-1])[1]]
+			)
 	}
 }
 
-bannerStyleSwitch.addEventListener('click', () => handleStyle(bannerStyles, bannerContent, bannerImage))
-alignmentStyleSwitch.addEventListener('click', () => handleStyle(alignmentStyles, bannerContent, bannerButton))
+bannerStyleSwitch.addEventListener('click', (e) => {
+	e.stopPropagation()
+	handleStyle(bannerStyles, bannerContent, bannerImage, bannerStyleCounter)
+	if(bannerStyleCounter < bannerStyles.length ){
+		bannerStyleCounter++
+	}else {
+		bannerStyleCounter = 0
+	}
+})
+alignmentStyleSwitch.addEventListener('click', (e) => {
+	e.stopPropagation()
+	console.log(bannerAlignmentCounter)
+	handleStyle(alignmentStyles, bannerContent, bannerButton, bannerAlignmentCounter)
+	if(bannerAlignmentCounter < bannerStyles.length ){
+		bannerAlignmentCounter++
+	}else {
+		bannerAlignmentCounter = 0
+	}
+})
