4ff1634729d209caedd472bc8d3ac29ee49f1ce7
Innuendo416 <collinstone2291@icloud.com>
2021-03-03 09:05:16 -0500

    Made that style changing function pure

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
-let bannerContent = document.getElementById('bannerContent')
-let bannerImage = document.getElementById('bannerImage')
-let bannerStyleButton = document.getElementById('bannerStyleButton')
-let bannerStyles = [
-	{
-		content: 'content--altOrder',
-		image: 'image--altOrder'
-	},
+const bannerContent = document.getElementById('bannerContent')
+const bannerImage = document.getElementById('bannerImage')
+const bannerButton = document.getElementById('bannerButton')
+const bannerStyleSwitch = document.getElementById('bannerStyleSwitch')
+const alignmentStyleSwitch = document.getElementById('bannerAlignmentSwitch')
+const bannerStyles = [
 	{
 		content: 'content--foreground',
 		image: 'image--background',
 	},
 	{
 		content: ['content--foreground', 'content--foreground--alt'],
-		image: ['image--background', 'image--background--circle']
+		image: ['image--background']
+	}
+]
+
+const alignmentStyles = [
+	{
+		content: ''
 	}
 ]
 
 let styleCounter = 0
-const handleStyle = (bannerStyles) => {
+const handleStyle = (styles, element1, element2) => {
+
 	const removeClass = (element1, element2, index) => {
-		if(typeof bannerStyles[index].content !== 'string'){
-			bannerStyles[index].content.forEach((c, i) => {
+		if(typeof styles[index][Object.keys(styles[index])[0]] !== 'string'){
+			styles[index][Object.keys(styles[index])[0]].forEach((c, i) => {
 				element1.classList.remove(c)
 			})
-			bannerStyles[index].image.forEach((c, i) => {
+			styles[index][Object.keys(styles[index])[1]].forEach((c, i) => {
 				element2.classList.remove(c)
 			})
-		} else {
-			element1.classList.remove(bannerStyles[index].content)
-			element2.classList.remove(bannerStyles[index].image)
+		}else {
+			element1.classList.remove(styles[index][Object.keys(styles[index])[0]])
+			element2.classList.remove(styles[index][Object.keys(styles[index])[1]])
 		}
 	}
 	const addClass = (element1, element2, index) => {
-		if(typeof bannerStyles[index].content !== 'string'){
-			bannerStyles[index].content.forEach((c, i) => {
+		if(typeof styles[index][Object.keys(styles[index])[0]] !== 'string'){
+			styles[index][Object.keys(styles[index])[0]].forEach((c, i) => {
 				element1.classList.add(c)
 			})
-			bannerStyles[index].image.forEach((c, i) => {
+			styles[index][Object.keys(styles[index])[1]].forEach((c, i) => {
 				element2.classList.add(c)
 			})
-		} else {
-			element1.classList.add(bannerStyles[index].content)
-			element2.classList.add(bannerStyles[index].image)
+		}else {
+			element1.classList.add(styles[index][Object.keys(styles[index])[0]])
+			element2.classList.add(styles[index][Object.keys(styles[index])[1]])
 		}
 	}
-	if(styleCounter < bannerStyles.length){
-		addClass(bannerContent, bannerImage, styleCounter)
+
+	if(styleCounter < styles.length){
 		if(styleCounter !== 0){
-			removeClass(bannerContent, bannerImage, styleCounter - 1)		
+			removeClass(element1, element2, styleCounter - 1)		
 		}
+		addClass(element1, element2, styleCounter)
 		styleCounter++
 	} else {
-		removeClass(bannerContent, bannerImage, bannerStyles.length - 1)
+		removeClass(element1, element2, styleCounter - 1)
 		styleCounter = 0
 	}
 }
 
-bannerStyleButton.addEventListener('click', () => handleStyle(bannerStyles))
+bannerStyleSwitch.addEventListener('click', () => handleStyle(bannerStyles, bannerContent, bannerImage))
+alignmentStyleSwitch.addEventListener('click', () => handleStyle(alignmentStyles, bannerContent, bannerButton))
