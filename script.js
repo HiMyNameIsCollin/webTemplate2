d3ab577e7b5339bed3be73226c1dcc398d3ef4c2
Innuendo416 <collinstone2291@icloud.com>
2021-03-04 14:06:30 -0500

    Catchup

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const banner = document.querySelector('.banner')
-const bannerContent = document.getElementById('bannerContent')
-const bannerImage = document.getElementById('bannerImage')
+const bannerContent = banner.children[0]
+const bannerImage = banner.children[1]
 const bannerButton = document.getElementById('bannerButton')
 const bannerStyleSwitch = document.getElementById('bannerStyleSwitch')
 const bannerAlignmentSwitch = document.getElementById('bannerAlignmentSwitch')
 const bannerColorSwitch = document.getElementById('bannerColorSwitch')
+const bannerOrientationSwitch = document.getElementById('bannerOrientationSwitch')
+const bannerImagePositionSwitch = document.getElementById('bannerImagePositionSwitch')
 
 const bannerStyles = [
 	{
@@ -27,6 +29,13 @@ const bannerStyles = [
 		content: ['content--foreground', 'content--foreground--alt', 'content--foreground--alt--right'],
 		image: ['image--background']
 	},
+]
+
+const orientationStyles = [
+	{
+		content: ['content--altOrder'],
+		image: ['image--altOrder']
+	}
 ]
 
 const alignmentStyles = [
@@ -67,13 +76,33 @@ const colorStyles = [
 	},
 ]
 
+const imagePositionStyles = [
+	{
+		image: ['image--imgPos1']
+	},
+	{
+		image: ['image--imgPos2']
+	},
+	{
+		image: ['image--imgPos3']
+	},
+	{
+		image: ['image--imgPos4']
+	},
+	{
+		image: ['image--imgPos5']
+	}
+]
+
 
 
 let bannerStyleCounter = 0
 let bannerAlignmentCounter = 0
 let bannerColorCounter = 0
-const handleStyle = (styles, element1, element2, counter) => {
-	const removeClass = (element1, element2, style1, style2) => {
+let bannerOrientationCounter = 0
+let bannerImagePositionCounter = 0
+const handleStyle = (styles, counter, element1, element2) => {
+	const removeClass = (style1, style2, element1, element2) => {
 		if(style1){
 			style1.forEach((c, i) => {
 				element1.classList.remove(c)
@@ -86,7 +115,7 @@ const handleStyle = (styles, element1, element2, counter) => {
 		}
 
 	}
-	const addClass = (element1, element2, style1, style2) => {
+	const addClass = (style1, style2, element1, element2 ) => {
 		if(style1){
 			style1.forEach((c, i) => {
 				element1.classList.add(c)
@@ -102,49 +131,49 @@ const handleStyle = (styles, element1, element2, counter) => {
 	if(counter < styles.length){
 		if(counter !== 0){
 			removeClass(
+				styles[counter-1][Object.keys(styles[counter-1])[0]], 
+				styles[counter-1][Object.keys(styles[counter-1])[1]],
 				element1, 
 				element2, 
-				styles[counter-1][Object.keys(styles[counter-1])[0]], 
-				styles[counter-1][Object.keys(styles[counter-1])[1]]
 			)		
 		}
 		addClass(
+				styles[counter][Object.keys(styles[counter])[0]], 
+				styles[counter][Object.keys(styles[counter])[1]],
 				element1, 
 				element2, 
-				styles[counter][Object.keys(styles[counter])[0]], 
-				styles[counter][Object.keys(styles[counter])[1]]
 			)
 	} else {
 		removeClass(
+				styles[counter-1][Object.keys(styles[counter-1])[0]], 
+				styles[counter-1][Object.keys(styles[counter-1])[1]],
 				element1, 
 				element2, 
-				styles[counter-1][Object.keys(styles[counter-1])[0]], 
-				styles[counter-1][Object.keys(styles[counter-1])[1]]
 			)
 	}
 }
 
-bannerStyleSwitch.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(bannerStyles, bannerContent, bannerImage, bannerStyleCounter)
+const handleBanner = (e) => {
+	handleStyle(bannerStyles, bannerStyleCounter, bannerContent, bannerImage)
 	if(bannerStyleCounter < bannerStyles.length ){
 		bannerStyleCounter++
 	}else {
 		bannerStyleCounter = 0 
 	}
-})
-bannerImage.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(bannerStyles, bannerContent, bannerImage, bannerStyleCounter)
-	if(bannerStyleCounter < bannerStyles.length ){
-		bannerStyleCounter++
-	}else {
-		bannerStyleCounter = 0 
+	if(bannerStyleCounter < 2){
+		bannerOrientationSwitch.classList.remove('switch--hidden')
+	} else if (bannerStyleCounter === 2) {
+		bannerOrientationSwitch.classList.add('switch--hidden')
 	}
-})
+}
+
+bannerStyleSwitch.addEventListener('click', handleBanner)
+bannerImage.addEventListener('click', handleBanner)
+
+
 bannerAlignmentSwitch.addEventListener('click', (e) => {
 	e.stopPropagation()
-	handleStyle(alignmentStyles, bannerContent, bannerButton, bannerAlignmentCounter)
+	handleStyle(alignmentStyles, bannerAlignmentCounter, bannerContent, bannerButton)
 	if(bannerAlignmentCounter < alignmentStyles.length ){
 		bannerAlignmentCounter++
 	}else {
@@ -153,10 +182,29 @@ bannerAlignmentSwitch.addEventListener('click', (e) => {
 })
 bannerColorSwitch.addEventListener('click', (e) => {
 	e.stopPropagation()
-	handleStyle(colorStyles, bannerContent, bannerImage, bannerColorCounter)
+	handleStyle(colorStyles, bannerColorCounter , bannerContent, bannerImage)
 	if(bannerColorCounter < colorStyles.length ){
 		bannerColorCounter++
 	}else {
 		bannerColorCounter = 0
+	}
+})
+bannerOrientationSwitch.addEventListener('click', (e) => {
+	e.stopPropagation()
+	handleStyle(orientationStyles, bannerOrientationCounter, bannerContent, bannerImage)
+	if(bannerOrientationCounter < bannerOrientationStyles.length ){
+		bannerOrientationCounter++
+	}else {
+		bannerOrientationStyles = 0
+	}
+})
+
+bannerImagePositionSwitch.addEventListener('click', (e) => {
+	e.stopPropagation()
+	handleStyle(imagePositionStyles, bannerImagePositionCounter, bannerImage)
+	if(bannerImagePositionCounter < imagePositionStyles.length ){
+		bannerImagePositionCounter++
+	}else {
+		bannerImagePositionCounter = 0
 	}
 })
