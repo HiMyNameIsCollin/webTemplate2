89944c64909a4257110697c7587261e3d0bb5619
Innuendo416 <collinstone2291@icloud.com>
2021-03-05 15:48:37 -0500

    Redid styles, and finally figured out constructors/this

--- script.js
+++ script.js
@@ -1,106 +1,156 @@
-const banner = document.querySelector('.banner')
-const bannerContent = banner.children[0]
-const bannerImage = banner.children[1]
-const bannerButton = document.getElementById('bannerButton')
-const bannerStyleSwitch = document.getElementById('bannerStyleSwitch')
-const bannerAlignmentSwitch = document.getElementById('bannerAlignmentSwitch')
-const bannerColorSwitch = document.getElementById('bannerColorSwitch')
-const bannerOrientationSwitch = document.getElementById('bannerOrientationSwitch')
-const bannerImagePositionSwitch = document.getElementById('bannerImagePositionSwitch')
-
-const bannerStyles = [
-	{
-		content: [],
-		image: ['image--circle']
-	},
-	{
-		content: ['content--foreground'],
-		image: ['image--background']
-	},
-	{
-		content: ['content--foreground'],
-		image: ['image--background', 'image--background--cover']
-	},
-	{
-		content: ['content--foreground', 'content--foreground--alt', 'content--foreground--alt--bottom'],
-		image: ['image--background']
-	},
-	{
-		content: ['content--foreground', 'content--foreground--alt', 'content--foreground--alt--right'],
-		image: ['image--background']
-	},
+const panelStyles = [
+	[
+		[],
+		['image--circle']
+	],
+	[
+		['content--front'],
+		['image--back']
+	],
+	[
+		['content--front', 'content--front--full'],
+		['image--back']
+	],
+	[
+		['content--front'],
+		['image--back', 'image--back--cover']
+	],
 ]
-
 const orientationStyles = [
-	{
-		content: ['content--altOrder'],
-		image: ['image--altOrder']
-	}
+	[
+		['content--altOrder'],
+		['image--altOrder', ]
+	]
+]
+const contentPositionStyles = [
+	[
+		['content--front--altLeft'],
+		[]
+	],
+	[
+		['content--front--left'],
+		[]
+	],
+	[
+		['content--front--altTop'],
+		[]
+	],
+	[
+		['content--front--top'],
+		[]
+	],
+	[
+		['content--front--altRight'],
+		[]
+	],
+	[
+		['content--front--right'],
+		[]
+	],
+	[
+		['content--front--altBottom'],
+		[]
+	],
+	[
+		['content--front--bottom'],
+		[]
+	],
 ]
 
 const alignmentStyles = [
-	{
-		content: ['content--alignCenter'],
-		button: ['content__button--alignCenter']
-	},
-	{
-		content: ['content--alignEnd'],
-		button: ['content__button--alignRight']
-	},
+	[
+		['content--alignCenter'],
+		[]
+	],
+	[
+		['content--alignEnd'],
+		[]
+	],
 ]
-
 const colorStyles = [
-	{
-		content: ['content--black'],
-		image: ['image--black']
-	},
-	{
-		content: ['content--purple'],
-		image: ['image--purple']
-	},
-	{
-		content: ['content--pink'],
-		image: ['image--pink']
-	},
-	{
-		content: ['content--yellow'],
-		image: ['image--yellow']
-	},
-	{
-		content: ['content--blue'],
-		image: ['image--blue']
-	},
-	{
-		content: ['content--teal'],
-		image: ['image--teal']
-	},
+	[
+		 ['content--black'],
+		 ['image--black']
+	],
+	[
+		 ['content--purple'],
+		 ['image--purple']
+	],
+	[
+		 ['content--pink'],
+		 ['image--pink']
+	],
+	[
+		 ['content--yellow'],
+		 ['image--yellow']
+	],
+	[
+		 ['content--blue'],
+		 ['image--blue']
+	],
+	[
+		 ['content--teal'],
+		 ['image--teal']
+	],
 ]
-
 const imagePositionStyles = [
-	{
-		image: ['image--imgPos1']
-	},
-	{
-		image: ['image--imgPos2']
-	},
-	{
-		image: ['image--imgPos3']
-	},
-	{
-		image: ['image--imgPos4']
-	},
-	{
-		image: ['image--imgPos5']
-	}
+	[
+		['image--imgPos1']
+	],
+	[
+		['image--imgPos2']
+	],
+	[
+		['image--imgPos3']
+	],
+	[
+		['image--imgPos4']
+	],
+	[
+		['image--imgPos5']
+	],
 ]
 
+function Panel(element) {
+	this.content = element.children[0]
+	this.image = element.children[1]
+	this.styleSwitch = element.children[0].children[1].children[0]
+	this.alignmentSwitch = element.children[0].children[1].children[1]
+	this.colorSwitch = element.children[0].children[1].children[2]
+	this.orientationSwitch = element.children[0].children[1].children[3]
+	this.imagePositionSwitch = element.children[0].children[1].children[4]
+	this.contentPositionSwitch = element.children[0].children[1].children[5]
+	this.styleCounter = 0
+	this.alignmentCounter = 0
+	this.colorCounter = 0
+	this.orientationCounter = 0
+	this.imagePositionCounter = 0
+	this.contentPositionCounter = 0
+	this.image.addEventListener('click', () => {handleStyle(panelStyles, this.styleCounter, this.content, this.image), this.styleCounter < panelStyles.length ? this.styleCounter++ : this.styleCounter = 0, this.watchStyles() })
+	this.styleSwitch.addEventListener('click', () => {handleStyle(panelStyles, this.styleCounter, this.content, this.image), this.styleCounter < panelStyles.length ? this.styleCounter++ : this.styleCounter = 0, this.watchStyles()})
+	this.alignmentSwitch.addEventListener('click', () => {handleStyle(alignmentStyles, this.alignmentCounter, this.content), this.alignmentCounter < alignmentStyles.length ? this.alignmentCounter++ : this.alignmentCounter = 0, this.watchStyles()})
+	this.colorSwitch.addEventListener('click', () => {handleStyle(colorStyles, this.colorCounter, this.content, this.image), this.colorCounter < colorStyles.length ? this.colorCounter++ : this.colorCounter = 0, this.watchStyles()})
+	this.orientationSwitch.addEventListener('click', () => {handleStyle(orientationStyles, this.orientationCounter, this.content, this.image), this.orientationCounter < orientationStyles.length ? this.orientationCounter++ : this.orientationCounter = 0, this.watchStyles()})
+	this.imagePositionSwitch.addEventListener('click', () => {handleStyle(imagePositionStyles, this.imagePositionCounter, this.image), this.imagePositionCounter < imagePositionStyles.length ? this.imagePositionCounter++ : this.imagePositionCounter = 0, this.watchStyles()})
+	this.contentPositionSwitch.addEventListener('click', () => {handleStyle(contentPositionStyles, this.contentPositionCounter, this.content), this.contentPositionCounter < contentPositionStyles.length ? this.contentPositionCounter++ : this.contentPositionCounter = 0, this.watchStyles()})
+	this.watchStyles = function(){
+		if(this.styleCounter < 2){
+			this.orientationSwitch.classList.remove('switch--hidden')
+		} else if (this.styleCounter === 2) {
+			this.orientationSwitch.classList.add('switch--hidden')
+		}
+		if(this.styleCounter > 1){
+			this.contentPositionSwitch.classList.remove('switch--hidden')
+		} else if(this.styleCounter === 0 ){
+			this.contentPositionSwitch.classList.add('switch--hidden')
+		}
+	}
+}
 
+const banner = new Panel(document.getElementById('banner'))
+const spotlight1 = new Panel(document.getElementById('spotlight1'))
+const spotlight2 = new Panel(document.getElementById('spotlight2'))
 
-let bannerStyleCounter = 0
-let bannerAlignmentCounter = 0
-let bannerColorCounter = 0
-let bannerOrientationCounter = 0
-let bannerImagePositionCounter = 0
 const handleStyle = (styles, counter, element1, element2) => {
 	const removeClass = (style1, style2, element1, element2) => {
 		if(style1){
@@ -113,7 +163,6 @@ const handleStyle = (styles, counter, element1, element2) => {
 				element2.classList.remove(c)
 			})		
 		}
-
 	}
 	const addClass = (style1, style2, element1, element2 ) => {
 		if(style1){
@@ -131,80 +180,24 @@ const handleStyle = (styles, counter, element1, element2) => {
 	if(counter < styles.length){
 		if(counter !== 0){
 			removeClass(
-				styles[counter-1][Object.keys(styles[counter-1])[0]], 
-				styles[counter-1][Object.keys(styles[counter-1])[1]],
+				styles[counter-1][0], 
+				styles[counter-1][1],
 				element1, 
 				element2, 
 			)		
 		}
 		addClass(
-				styles[counter][Object.keys(styles[counter])[0]], 
-				styles[counter][Object.keys(styles[counter])[1]],
+				styles[counter][0], 
+				styles[counter][1],
 				element1, 
 				element2, 
 			)
 	} else {
 		removeClass(
-				styles[counter-1][Object.keys(styles[counter-1])[0]], 
-				styles[counter-1][Object.keys(styles[counter-1])[1]],
+				styles[counter-1][0], 
+				styles[counter-1][1],
 				element1, 
 				element2, 
 			)
 	}
 }
-
-const handleBanner = (e) => {
-	handleStyle(bannerStyles, bannerStyleCounter, bannerContent, bannerImage)
-	if(bannerStyleCounter < bannerStyles.length ){
-		bannerStyleCounter++
-	}else {
-		bannerStyleCounter = 0 
-	}
-	if(bannerStyleCounter < 2){
-		bannerOrientationSwitch.classList.remove('switch--hidden')
-	} else if (bannerStyleCounter === 2) {
-		bannerOrientationSwitch.classList.add('switch--hidden')
-	}
-}
-
-bannerStyleSwitch.addEventListener('click', handleBanner)
-bannerImage.addEventListener('click', handleBanner)
-
-
-bannerAlignmentSwitch.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(alignmentStyles, bannerAlignmentCounter, bannerContent, bannerButton)
-	if(bannerAlignmentCounter < alignmentStyles.length ){
-		bannerAlignmentCounter++
-	}else {
-		bannerAlignmentCounter = 0
-	}
-})
-bannerColorSwitch.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(colorStyles, bannerColorCounter , bannerContent, bannerImage)
-	if(bannerColorCounter < colorStyles.length ){
-		bannerColorCounter++
-	}else {
-		bannerColorCounter = 0
-	}
-})
-bannerOrientationSwitch.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(orientationStyles, bannerOrientationCounter, bannerContent, bannerImage)
-	if(bannerOrientationCounter < bannerOrientationStyles.length ){
-		bannerOrientationCounter++
-	}else {
-		bannerOrientationStyles = 0
-	}
-})
-
-bannerImagePositionSwitch.addEventListener('click', (e) => {
-	e.stopPropagation()
-	handleStyle(imagePositionStyles, bannerImagePositionCounter, bannerImage)
-	if(bannerImagePositionCounter < imagePositionStyles.length ){
-		bannerImagePositionCounter++
-	}else {
-		bannerImagePositionCounter = 0
-	}
-})
